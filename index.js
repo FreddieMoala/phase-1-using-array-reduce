3f6c0bc83afe28c05825a2babecf30b2733cc5bc
Siaosi Moala <loto_toa@hotmail.com>
2024-03-25 13:51:26 +1100

    DONE

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// function totalBatteries(array, func, initVal){
+//     let accum = initVal;
+//     for ( const element of array){
+//         accum = func(element, accum)
+//     }
+//     return accum;
+// }
+
+// function sumBatteries(element, accum){
+//     return element + accum;
+// }
+
+// console.log(totalBatteries(batteryBatches, sumBatteries, 0));
+
+const totalBatteries = batteryBatches.reduce((total, element) => element + total, 0);
